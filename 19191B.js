a130e8d1b1bddb47ba1c63fcad15b22301b12fee
Jeremy Helms <digitaljhelms@gmail.com>
2024-01-09 23:38:44 -0600

    #19191B Sticky

--- 19191B.js
+++ 19191B.js
@@ -1,1 +1,11 @@
-// Silence is golden
+window.addEventListener('scroll', () => {
+    const stickynav = document.querySelector('.sticky')
+
+    if (window.scrollY > (stickynav.offsetTop + (stickynav.offsetHeight - 1))) {
+        stickynav.nextElementSibling.style['padding-top'] = stickynav.offsetHeight + 'px';
+        stickynav.classList.add('stuck')
+    } else {
+        stickynav.nextElementSibling.removeAttribute('style')
+        stickynav.classList.remove('stuck')
+    }
+})
